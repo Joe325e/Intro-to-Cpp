2ca4b01eb4a92c58c52fc8f66a4f9b1320ba7b51
Josiah <Josiah.richarson50@gmail.com>
2020-11-06 16:47:47 -0800

    Intro to C++ assessment

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    // modern C++ - C++11/14/17+
+    std::cout << "Hello Modern World";
+
+    // C++ / classic C++ - C++98/03
+    printf("The number %d is the best number.", 5);
+
+    return 0;
+}
+
